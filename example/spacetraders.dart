702589d65a07118cac29594845065098899c0188
Chris Sells <csells@sellsbrothers.com>
2023-05-08 18:03:56 -0700

    tweaks

--- example/spacetraders.dart
+++ example/spacetraders.dart
@@ -1,70 +1,16 @@
 import 'package:intl/intl.dart';
 import 'package:spacetraders_dart/spacetraders_dart.dart';
 import 'package:tabular/tabular.dart';
-// import 'package:tabular/tabular.dart';
 
 void main(List<String> args) async {
-  // final status = await defaultApiClient.gameStatus();
-  final json = '''
-{
-  "status": "SpaceTraders is currently online and available to play",
-  "stats": {
-    "agents": 1802,
-    "ships": 2833,
-    "systems": 5000,
-    "waypoints": 25172
-  },
-  "leaderboards": {
-    "mostCredits": [
-      {
-        "agentSymbol": "FERB287",
-        "credits": 79960656
-      },
-      {
-        "agentSymbol": "RIHAN",
-        "credits": 71797006
-      },
-      {
-        "agentSymbol": "FEBA0000",
-        "credits": 22865909
-      },
-      {
-        "agentSymbol": "MAYLEEN",
-        "credits": 4485900
-      },
-      {
-        "agentSymbol": "GILBERVF",
-        "credits": 1039988
-      }
-    ],
-    "mostSubmittedCharts": [
-      {
-        "agentSymbol": "RIHAN",
-        "chartCount": 14976
-      },
-      {
-        "agentSymbol": "HEDGEHOG",
-        "chartCount": 3216
-      },
-      {
-        "agentSymbol": "FEBA0000",
-        "chartCount": 1177
-      },
-      {
-        "agentSymbol": "FEBACHART02",
-        "chartCount": 15
-      },
-      {
-        "agentSymbol": "FEBACHART08",
-        "chartCount": 13
-      }
-    ]
-  }
-}''';
-  // todo: check out the `format` and `tabular` packages
+  final status = await defaultApiClient.gameStatus();
+
+  // todo: check out the `format` package
   final nf = NumberFormat('#,##0');
-  final status = GameStatus200Response.fromJson(json);
-  print('status: ${status.status}');
+
+  print('# status');
+  print('');
+  print('${status.status}');
 
   final stats = [
     ['stat', '#'],
@@ -75,11 +21,13 @@ void main(List<String> args) async {
   ];
 
   print('');
-  print('stats:');
+  print('# stats');
+  print('');
   print(tabular(stats, align: {'#': Side.end}));
 
   print('');
-  print('most credits:');
+  print('# most credits');
+  print('');
   final credits = List<List<String>>.from([
     ['agent', 'credits'],
     ...status.leaderboards.mostCredits
@@ -88,7 +36,8 @@ void main(List<String> args) async {
   print(tabular(credits, align: {'credits': Side.end}));
 
   print('');
-  print('most submitted charts:');
+  print('# most submitted charts');
+  print('');
   final charts = List<List<String>>.from([
     ['agent', 'count'],
     ...status.leaderboards.mostSubmittedCharts
